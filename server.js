91f1888eb1a43e22acc6a78cf1ad0947a0c9c6dc
msolorio <msolorio789@gmail.com>
2017-12-02 15:07:40 -0800

    add morgan logging middleware

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
 const wiki = require('./wiki');
+
+// sets up morgan logging for all requests to app
+app.use(logger('dev'));
+
+// route requests to /wiki to wiki router
+app.use('/wiki', wiki);
+
 
 app.get('/', (req, res) => {
   res.send('hello expresso');
 });
-
-// route requests to /wiki to wiki router
-app.use('/wiki', wiki);
 
 app.listen(3000, () => {
   console.log('your express app is running on port 3000');
