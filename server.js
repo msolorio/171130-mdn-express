e0830ff889eedff742d0b51477541c06494e64fb
msolorio <msolorio789@gmail.com>
2017-12-02 16:38:22 -0800

    add error handling

--- server.js
+++ server.js
@@ -9,11 +9,24 @@ app.use(logger('dev'));
 // route requests to /wiki to wiki router
 app.use('/wiki', wiki);
 
+app.use('/unknown', (req, res, next) => {
+  const error = new Error('request made to /unknown');
+
+  next(error);
+});
+
 // when a request for static assets is made to the root serve public
 app.use(express.static('public'));
 
 // when a request for static assets is made to /wiki serve public
 app.use('/wiki', express.static('public'));
+
+app.use((err, req, res, next) => {
+  console.log('error:', err);
+  console.error('err.stack:', err.stack);
+
+  res.status(500).send('something broke');
+});
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
